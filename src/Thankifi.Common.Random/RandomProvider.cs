23e89b887bd359f69a9d2cb3b3a013d5650c85d0
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-08-01 14:25:11 +0300

    feat: implement random provider

---
+++ src/Thankifi.Common.Random/RandomProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace Thankifi.Common.Random
+{
+    /// <summary>
+    ///     Random provider thanks to https://csharpindepth.com/Articles/Random
+    /// </summary>
+    public static class RandomProvider
+    {
+        private static int _seed = Environment.TickCount;
+
+        private static readonly ThreadLocal<System.Random> RandomWrapper = new(() => new System.Random(Interlocked.Increment(ref _seed)));
+
+        /// <summary>
+        ///     Retrieve a local (as in thread) random provider.
+        /// </summary>
+        /// <returns>An instance of <see cref="Random" />.</returns>
+        public static System.Random? GetThreadRandom()
+        {
+            return RandomWrapper.Value;
+        }
+    }
+}
